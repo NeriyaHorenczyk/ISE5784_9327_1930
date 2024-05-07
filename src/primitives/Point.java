9b1d933e3481852ae42f3dce5d8b5855efde6350
ben <benzigolds@gmail.com>
2024-05-07 19:06:35 +0300

    new calss fp point

---
+++ src/primitives/Point.java
@@ -0,0 +1,14 @@
+package primitives;
+
+public class Point {
+    protected Double3 xyz;
+
+    /**
+     *
+     * @param p
+     * @return the destans betwin the point and the val
+     */
+    public double distans(Point p){
+        return 0.0;
+    }
+}
