e86bc34bd340f53dddfbca3ea44fb677074c41b6
ben <benzigolds@gmail.com>
2024-05-08 18:09:35 +0300

    update vector

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -6,41 +6,41 @@ package primitives;
 public class Point
 {
 
-	protected Double3 xyz;
+    protected Double3 xyz;
 
-	/**
-	 * This is a static point that represent the (0,0,0) point.
-	 */
-	public static Point ZERO = new Point(Double3.ZERO);
+    /**
+     * This is a static point that represent the (0,0,0) point.
+     */
+    public static Point ZERO = new Point(Double3.ZERO);
 
-	/**
-	 * This is a parameter constructor
-	 *
-	 * @param xyz , Initialize the field to the parameter.
-	 */
-	public Point(Double3 xyz)
-	{
-		this.xyz = xyz;
-	}
+    /**
+     * This is a parameter constructor
+     *
+     * @param xyz , Initialize the field to the parameter.
+     */
+    public Point(Double3 xyz)
+    {
+        this.xyz = xyz;
+    }
 
-	public Point(double d1, double d2, double d3)
-	{
-		xyz = new Double3(d1, d2, d3);
-	}
+    public Point(double d1, double d2, double d3)
+    {
+        xyz = new Double3(d1, d2, d3);
+    }
 
-	public Vector substract(Point p)
-	{
-		Vector v = new Vector();
-		return v;
-	}
+    public Vector substract(Point p)
+    {
+        Vector v = new Vector();
+        return v;
+    }
 
 
-	/**
-	 * @param p
-	 * @return the destans betwin the point and the val
-	 */
-	public double distans(Point p)
-	{
-		return 0.0;
-	}
+    /**
+     * @param p
+     * @return the destans betwin the point and the val
+     */
+    public double distans(Point p)
+    {
+        return 0.0;
+    }
 }
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,5 +1,21 @@
 package primitives;
 
-public class Vector
+public class Vector extends Point
 {
+    public Vector(double d1,double d2,double d3) throws IllegalArgumentException{
+        super(d1,d2,d3);
+        Double3 testVectorZero=new Double3(d1,d2,d3);
+        if(testVectorZero.equals(Double3.ZERO))
+            throw new IllegalArgumentException ("vector is zero");
+
+
+
+    }
+
+    public Vector( Double3 dob3) throws IllegalArgumentException{
+        super(dob3);
+        if(dob3.equals(Double3.ZERO))
+            throw new IllegalArgumentException ("vector is zero");
+
+    }
 }
