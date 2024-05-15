c8b2cfbdd06d8710c301de2479dbda144387cb03
Horenczyk Neriya <horenczy@g.jct.ac.il>
2024-05-15 18:13:32 +0300

    added unit tests fot Polygon and Point

---
+++ unittests/primitives/PointTest.java
@@ -0,0 +1,39 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest
+{
+
+	@Test
+	void subtract()
+	{
+	}
+
+	@Test
+	void add()
+	{
+	}
+
+	@Test
+	void distanceSquared()
+	{
+	}
+
+	@Test
+	void distance()
+	{
+	}
+
+	@Test
+	void testEquals()
+	{
+	}
+
+	@Test
+	void testToString()
+	{
+	}
+}
