84c8af35869bbcdd5dc0a86f0df8c12185f9ade5
Horenczyk Neriya <horenczy@g.jct.ac.il>
2024-05-21 19:01:00 +0300

    added all the tests classes 2.0

---
+++ unittests/geometries/PlaneTests.java
@@ -0,0 +1,19 @@
+package geometries;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlaneTests
+{
+
+	@Test
+	void getNormal()
+	{
+	}
+
+	@Test
+	void testGetNormal()
+	{
+	}
+}
---
+++ unittests/primitives/VectorTests.java
@@ -0,0 +1,49 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+class VectorTests
+{
+
+	@Test
+	void Vector()
+	{
+
+	}
+
+	@Test
+	void lengthSquared()
+	{
+
+	}
+
+	@Test
+	void length()
+	{
+	}
+
+	@Test
+	void add()
+	{
+	}
+
+	@Test
+	void scale()
+	{
+	}
+
+	@Test
+	void dotProduct()
+	{
+	}
+
+	@Test
+	void crossProduct()
+	{
+	}
+
+	@Test
+	void normalize()
+	{
+	}
+}
