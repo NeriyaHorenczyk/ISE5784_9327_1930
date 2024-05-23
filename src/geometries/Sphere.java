f59e46b390c7bb1ae12e6245d163315bf43fe3af
ben <benzigolds@gmail.com>
2024-05-23 14:52:25 +0300

    finsh getNormal and test to sphera

--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -24,6 +24,7 @@ public class Sphere extends RadialGeometry
 	@Override
 	public Vector getNormal(Point p)
 	{
-		return null;
+
+		return p.subtract(center).normalize();
 	}
 }
--- unittests/geometries/SphereTests.java
+++ unittests/geometries/SphereTests.java
@@ -1,6 +1,11 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Vector;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SphereTests
 {
@@ -8,5 +13,17 @@ class SphereTests
 	@Test
 	void getNormal()
 	{
+		Point p1=new Point(0,0,2);
+		Point p2=new Point(0,0,1);
+		Sphere s1=new Sphere(1,p2);
+		Vector spheraNormai=s1.getNormal(p1);
+
+		assertTrue(spheraNormai.equals(new Vector(0,0,1))||
+				spheraNormai.equals(new Vector(0,0,-1)),
+				" the sphera normal is not equals to the vector ");
+
+		assertEquals(1,spheraNormai.length(),
+				" the normal lenght is not 1 ");
+
 	}
 }
