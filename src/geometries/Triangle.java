9c2f5f700ee06937df4f13f659fe6072ab9b037f
ben <benzigolds@gmail.com>
2024-06-04 18:52:59 +0300

    triangle intersctin

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -6,6 +6,8 @@ import primitives.Vector;
 
 import java.util.List;
 
+import static primitives.Util.alignZero;
+
 /**
  * Triangle class that inherit from Polygon class.
  * represent a new Triangle in space.
@@ -19,9 +21,17 @@ public class Triangle extends Polygon
 
 	public List<Point> findIntersections(Ray ray)
 	{
-
-
-		return super.findIntersections(ray);
+		Vector v1= vertices.get(0).subtract(ray.getHead());
+		Vector v2= vertices.get(1).subtract(ray.getHead());
+		Vector v3= vertices.get(2).subtract(ray.getHead());
+		Vector n1= v1.crossProduct(v2).normalize();
+		Vector n2= v2.crossProduct(v3).normalize();
+		Vector n3=v3.crossProduct(v1).normalize();
+		Vector direction=ray.getDirection();
+			if((alignZero(direction.dotProduct(n1))>0 )&&(alignZero(direction.dotProduct(n2))>0)&&(alignZero(direction.dotProduct(n3))>0)||
+					(alignZero(direction.dotProduct(n1))<0 )&&(alignZero(direction.dotProduct(n2))<0)&&(alignZero(direction.dotProduct(n3))<0))
+				return plane.findIntersections(ray);
+		return null ;
 	}
 
 }
